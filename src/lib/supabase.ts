c1e77ac0ad04085999b11203cd30a521e5dc85cb
Arsad Ansari <arsadsagir@gmail.com>
2026-02-18 05:37:38 +0000

    ## Error Type Build Error
    ## Error Message
    Export createMiddlewareClien

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error(
+    'Supabase credentials are not set. Please create a `.env.local` file in the root of your project and add your Supabase URL and Anon Key.\n\nExample:\nNEXT_PUBLIC_SUPABASE_URL=https://your-project-id.supabase.co\nNEXT_PUBLIC_SUPABASE_ANON_KEY=your-public-anon-key'
+  );
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+
+import { createServerClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
+  const supabase = createServerClient({ req, res })
 
   const {
     data: { session },
